3291693b9999128297efbd1cd94c39bf4e711657
T-taku <t-takuscratch@outlook.jp>
2024-06-03 22:19:32 +0900

    [Fix] エラー修正

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 import random
-from sqlalchemy import INTEGER, TIMESTAMP, Column, String, func
-from db import Base
+from sqlalchemy import INTEGER, TIMESTAMP, String, func
+from sqlalchemy.orm import Mapped, mapped_column, declarative_base
+from pydantic import BaseModel
 import pytz
 import string
 
+Base = declarative_base()
 jst = pytz.timezone("Asia/Tokyo")
 
 
@@ -16,28 +18,19 @@ def generate_payment_id(length=16):
 class User(Base):
     __tablename__ = "users"
 
-    user_mail = Column(String(31), primary_key=True)
-    user_name = Column(String(20))
-    user_class = Column(String(20))
-    user_role = Column(String(20))
+    user_mail: Mapped[str] = mapped_column(String(31), primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(20))
+    user_class: Mapped[str] = mapped_column(String(20))
+    user_role: Mapped[str] = mapped_column(String(20))
 
 
 class Setting(Base):
     __tablename__ = "setting"
 
-    class_name = Column(String(10), primary_key=True)
-    goal = Column(INTEGER)
-    reserve = Column(INTEGER)
-    additionalreserve = Column(INTEGER)
-
-
-class GetAllSetting(Setting):
-    class_name: str
-    goal: int
-    reserve: int
-
-    class Config:
-        orm_mode = True
+    class_name: Mapped[str] = mapped_column(String(10), primary_key=True)
+    goal: Mapped[int] = mapped_column(INTEGER)
+    reserve: Mapped[int] = mapped_column(INTEGER)
+    additionalreserve: Mapped[int] = mapped_column(INTEGER)
 
 
 class History(Base):
@@ -45,23 +38,30 @@ class History(Base):
     timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
     __tablename__ = "history"
 
-    payment_id = Column(
-        String(15),
-        primary_key=True,
-        default=str(generate_payment_id()),
-    )
-    paid_class = Column(String(16))
-    timestamp = Column(TIMESTAMP, default=func.now())
-    total = Column(INTEGER)
-    change = Column(INTEGER)
-    product = Column(String(255))
+    payment_id: Mapped[str] = mapped_column(String(15), primary_key=True, default=generate_payment_id)
+    paid_class: Mapped[str] = mapped_column(String(16))
+    timestamp: Mapped[datetime] = mapped_column(TIMESTAMP, default=func.now())
+    total: Mapped[int] = mapped_column(INTEGER)
+    change: Mapped[int] = mapped_column(INTEGER)
+    product: Mapped[str] = mapped_column(String(255))
 
 
-class Get_History_all(History):
+class GetAllSetting(BaseModel):
+    class_name: str
+    goal: int
+    reserve: int
+    additionalreserve: int
+
+    class Config:
+        orm_mode = True
+
+
+class GetHistoryAll(BaseModel):
+    payment_id: str
     paid_class: str
-    timestamp: str
-    total: str
-    change: str
+    timestamp: datetime
+    total: int
+    change: int
     product: str
 
     class Config:
